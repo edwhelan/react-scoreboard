10ba971532e67eb6d3d218d6b60d0a7172ca1e15
Edward Whelan <vroxing@gmail.com>
2018-12-03 11:25:48 -0500

    add increment function for score

--- src/App.js
+++ src/App.js
@@ -10,17 +10,17 @@ class App extends Component {
         {
           id: 1,
           name: 'alice',
-          score: 1001
+          score: 0
         },
         {
           id: 2,
           name: 'ahjuma',
-          score: 200
+          score: 0
         },
         {
           id: 3,
           name: 'wendy',
-          score: 199
+          score: 0
         }
       ]
     }
@@ -45,10 +45,30 @@ class App extends Component {
         <div key={score.id}>
           <h2 >{score.name}</h2>
           <h3>{score.score}</h3>
+          <button onClick={() => this._incrementScoreById(score.id)}>+</button>
         </div>
       );
     });
     return cards;
+  }
+  _incrementScoreById(id) {
+    // find player in this.state.scores
+    const newScores = this.state.scores.map(score => {
+      if (score.id !== id) {
+        return score;
+      } else {
+        return {
+          id: score.id,
+          name: score.name,
+          //increment score
+          score: score.score + 1
+        }
+      }
+    })
+    //and call this.setState
+    this.setState({
+      scores: newScores
+    })
   }
 
 }
