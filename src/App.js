e325276cbce2a24ae6f5d2e985d81e3935d38a92
Edward Whelan <vroxing@gmail.com>
2018-12-03 10:44:12 -0500

    add basic rendering for players

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    //set up state 
+    this.state = {
+      scores: [
+        {
+          id: 1,
+          name: 'alice',
+          score: 1001
+        },
+        {
+          id: 2,
+          name: 'ahjuma',
+          score: 200
+        },
+        {
+          id: 3,
+          name: 'wendy',
+          score: 199
+        }
+      ]
+    }
+  }
+
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Scoreboard App</h1>
+        <div className='score-container'>
+          {this._scoresAsCards()}
+        </div>
       </div>
     );
   }
+
+  _scoresAsCards() {
+    const cards = this.state.scores.map(score => {
+      return (
+        <div key={score.id}>
+          <h2 >{score.name}</h2>
+          <h3>{score.score}</h3>
+        </div>
+      );
+    });
+    return cards;
+  }
+
 }
 
 export default App;
